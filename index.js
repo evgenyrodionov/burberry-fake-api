b4bae9bf4db7522352f6b5c3025859762bfb8494
Evgeny Rodionov <git@erodionov.ru>
2017-09-05 20:40:54 +0300

    feat: add men-suits, replace id to slug

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 /* eslint-disable global-require */
 const express = require('express'); // eslint-disable-line import/newline-after-import
 const cors = require('cors');
+
 const collections = {
   men: {
-    clothing: require('./data/men-clothing.json'),
-    scarves: require('./data/men-scarves.json'),
-    accessories: require('./data/men-accessories.json'),
-    shoes: require('./data/men-shoes.json'),
-    fragrance: require('./data/men-fragrance.json'),
-    gifts: require('./data/men-gifts.json'),
+    suits: require('./data/men-suits.json'),
+    // blazers: require('./data/men-blazers.json'),
+    // shirts: require('./data/men-shirts.json'),
+    // 'dress-shirts': require('./data/men-dress-shirts.json'),
+    // knitwear: require('./data/men-knitwear.json'),
+    // sweatshirts: require('./data/men-sweatshirts.json'),
+    // polos: require('./data/men-polos.json'),
+    // trousers: require('./data/men-trousers.json'),
+    // jeans: require('./data/men-jeans.json'),
+    // swimwear: require('./data/men-swimwear.json'),
+    // underwear: require('./data/men-underwear.json'),
   },
-  women: {
-    clothing: require('./data/women-clothing.json'),
-    bags: require('./data/women-bags.json'),
-    scarves: require('./data/women-scarves.json'),
-    accessories: require('./data/women-accessories.json'),
-    shoes: require('./data/women-shoes.json'),
-    fragrance: require('./data/women-fragrance.json'),
-    gifts: require('./data/women-gifts.json'),
-  },
+  // women: {
+  //   clothing: require('./data/women-clothing.json'),
+  //   bags: require('./data/women-bags.json'),
+  //   scarves: require('./data/women-scarves.json'),
+  //   accessories: require('./data/women-accessories.json'),
+  //   shoes: require('./data/women-shoes.json'),
+  //   fragrance: require('./data/women-fragrance.json'),
+  //   gifts: require('./data/women-gifts.json'),
+  // },
 };
 
 const app = express();
@@ -29,22 +35,26 @@ router.get('/products/:group/:type', (req, res) => {
   const { limit = 32, offset = 0 } = req.query;
 
   if (collections[group] && collections[group][type]) {
-    const items = collections[group][type];
+    const { items, title, description } = collections[group][type];
 
     res.send({
+      title,
+      description,
       items: [...items].splice(offset, limit),
       total: items.length,
+      limit: Number(limit),
+      offset: Number(offset),
     });
   } else {
     res.sendStatus(404);
   }
 });
 
-router.get('/products/:group/:type/:id', (req, res) => {
-  const { group, type, id } = req.params;
+router.get('/products/:group/:type/:slug', (req, res) => {
+  const { group, type, slug } = req.params;
 
   if (collections[group] && collections[group][type]) {
-    const product = collections[group][type].find(p => p.id === id);
+    const product = collections[group][type].items.find(p => p.slug === slug);
 
     if (product) res.send(product);
     else res.sendStatus(404);
@@ -59,9 +69,10 @@ app.get('/', (req, res) => {
   res.send({
     docs: 'https://github.com/evgenyrodionov/burberry-fake-api',
     version: `1.0.0-${process.env.BUILD_DATE || 'local'}`,
+    routes: ['/v1/products/men/suits', '/v1/products/men/suits/:slug'],
   });
 });
 
 app.listen(3000, () => {
-  console.log('adidas-api listening on port 3000!'); // eslint-disable-line no-console
+  console.log('burberry-fake-api listening on port 3000!'); // eslint-disable-line no-console
 });
